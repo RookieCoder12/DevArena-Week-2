4e3dbb85cd7e16b45c9f916599039c32a83dea35
Bipranshu Baghel <136240352+RookieCoder12@users.noreply.github.com>
2026-02-16 14:19:49 +0530

    completed: grade_calculator.py

---
+++ grade_calculator.py
@@ -0,0 +1,26 @@
+name = str(input("Enter the name: "))
+marks = int(input("Enter the number: "))
+
+def grade_calculator(m : int):
+    if m > 100:
+        print("Invalid Score")
+    else:
+        if m in range(90, 101):
+            print("Grade: A")
+            print("Message: Excellent, keep up the good work.")
+        elif m in range (80, 90):
+            print("Grade: B")
+            print("Message: Very Good, Keep it up.")
+        elif m in range (70, 80):
+            print("Grade: C")
+            print("Message: Keep Going, You can improve.")
+        elif m in range (60, 70):
+            print("Grade: D")
+            print("Message: Put in some work.")
+        else:
+            print("Grade: E")
+            print("Message: You need to work a lot more harder.")
+
+print(f"📊 Result for {name.capitalize()}: ")
+print(f"Marks: {marks}")
+grade_calculator(marks)
